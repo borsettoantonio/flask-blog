d0a32e4e70ce5bb3facfdd09766d0c30c4199df4
antonio <borsettoantonio@gmai.com>
2025-03-06 10:29:53 +0100

    inserire più blocchi in una pagina base

---
+++ blog/routes.py
@@ -0,0 +1,10 @@
+from flask import render_template
+from blog import app
+
+@app.route('/')
+def homepage():
+    posts = [{"title": "primo post", "body": "random body"},
+             {"title": "secondo post", "body": "more random body"}]
+    some_boolean_flag = True
+    return render_template("homepage.html", posts=posts, 
+                           boolean_flag=some_boolean_flag)
---
+++ init.ps1
@@ -0,0 +1,4 @@
+$env:FLASK_APP="run.py"
+$env:FLASK_DEBUG=1
+$env:FLASK_APP
+$env:FLASK_DEBUG
